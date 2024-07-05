5682cce633e7d3425acb32e3a47eb42bfbc874d8
laemons <82925755+laemons@users.noreply.github.com>
2024-07-05 16:18:01 +0700

    update

--- randomFact.js
+++ randomFact.js
@@ -1,6 +1,13 @@
-const facts = ["1", "2", "3"];
-
-!function newFact() {
-  const randomFact = Math.floor(Math.random() * facts.length);
-  document.getElementById('factDisplay').innerHTML = facts[randomFact];
-}();
+const https = require("https");
+const factURL = "https://nekos.life/api/v2/fact";
+https.get(`${factURL}`, data => {
+  console.log("\n");
+  data.on("data", chunk => {
+    let Fact = JSON.parse(chunk.toString());
+    //reply(Fact.fact)
+    console.log(Fact.fact,"\n");
+  });
+  data.on("error", err => {
+    console.error(`Error: ${err}`);
+  });
+});
