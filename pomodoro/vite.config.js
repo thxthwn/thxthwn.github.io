ec30e1b85dbfac2d793197a624677f6d327c79af
thxthwn <therealvampy@gmail.com>
2025-12-21 10:26:43 +0700

    Update vite.config.js

--- pomodoro/vite.config.js
+++ pomodoro/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/thxthwn.github.io/pomodoro/", // ← your repo name
+    base: "/pomodoro/", // ← your repo name
 });
